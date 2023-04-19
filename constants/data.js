2e9eaf76141385242e9ef2a46bdb03a376cb8233
zahranrahamth <64359869+zahranrahamth@users.noreply.github.com>
2023-04-19 15:45:13 +0530

    Update data.js

--- constants/data.js
+++ constants/data.js
@@ -45,7 +45,7 @@ const userData = {
     {
       title: "FlyHyer - Surge Intership 2022 (Practise Website)",
       link: "https://flyhyer.com",
-      imgUrl: "/flyhyer.png",
+      imgUrl: "",
     },
   ],
   about: {
